a1c8af6eb220058362d041474f43a4a9a879cb44
root <root@TRNCLILNXNATEK02.jnittraining.com>
2017-09-07 18:10:57 -0400

    secondcommit

--- nt/recipes/default.rb
+++ nt/recipes/default.rb
@@ -1,5 +1,28 @@
 #
-# Cookbook:: nt
+# Cookbook Name:: httpd
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+package 'httpd' do
+  action :install
+end
+
+service 'httpd' do
+  action [ :enable, :start ]
+end
+
+cookbook_file "/var/www/html/index.html" do
+  source "index.html"
+  mode "0644"
+end
+
+execute 'httpd_firewall' do
+  command '/usr/bin/firewall-cmd  --permanent --zone public --add-service http'
+  ignore_failure true
+end
+
+execute 'reload_firewall' do
+  command '/usr/bin/firewall-cmd --reload'
+  ignore_failure true
+end
